b1b3101d783e87307d9810d4001c2eb2933c18b4
Alan Zhang <alanzhang93@gmail.com>
2019-01-18 22:56:31 -0500

    Added api key input support

--- coursescheduler/coursescheduler.py
+++ coursescheduler/coursescheduler.py
@@ -1,7 +1,14 @@
 from interface import setgui
+import os
 
+def setapikey():
+    with open('apikey.txt') as f:
+    key = f.read()
+    if key is not None and key != '':
+        os.environ["UW_API_KEY"] = key
 
 def main():
+    setapikey()
     setgui()
 
 if __name__ == '__main__':
